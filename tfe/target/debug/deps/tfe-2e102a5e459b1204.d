c2cce8ca83d377b9bf2f7c9cf772f6515727cbe5
LuukHenk <LuukPerdaems@hotmail.com>
2020-03-12 12:34:09 +0100

    ...

---
+++ tfe/target/debug/deps/tfe-2e102a5e459b1204.d
@@ -0,0 +1,5 @@
+/home/zoutekaas/Dropbox/home/github/2048-Solver/tfe/target/debug/deps/tfe-2e102a5e459b1204: src/main.rs
+
+/home/zoutekaas/Dropbox/home/github/2048-Solver/tfe/target/debug/deps/tfe-2e102a5e459b1204.d: src/main.rs
+
+src/main.rs:
---
+++ tfe/target/debug/tfe.d
@@ -0,0 +1,1 @@
+/home/zoutekaas/Dropbox/home/github/2048-Solver/tfe/target/debug/tfe: /home/zoutekaas/Dropbox/home/github/2048-Solver/tfe/src/main.rs
