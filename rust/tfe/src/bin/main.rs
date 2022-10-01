15d7c8f8ef42f7f53c2b07fdfb0029fe05f017df
Zoutekaas <luukperdaems@Hotmail.com>
2022-10-01 10:00:52 +0200

    small cleanup

--- rust/tfe/src/bin/main.rs
+++ rust/tfe/src/bin/main.rs
@@ -1,20 +1,3 @@
-use tfe::Game;
-use tfe::Statistics;
 use tfe::Player;
-use stopwatch::{Stopwatch};
 
-fn main() {
-    // Arrange
-    let stopwatch = Stopwatch::start_new();
-    let total_games: u32 = 10;
-    let workers: u32 = 1;
-
-    // Play games
-    Player::train(total_games, workers, 0_u8);
-    
-    // Display result
-    // println!("Total games: {:?}\nTime: {:?}", played_games.len(), stopwatch.elapsed());
-    // println!("------------------------");
-    // println!("Game_data: {:?}", played_games);
-    // Statistics::print_highest_tiles_statistics(highest_tiles);
-}
+fn main() {Player::train(10_u32, 1_u32, 0_u8);}
--- rust/tfe/src/player.rs
+++ rust/tfe/src/player.rs
@@ -1,13 +1,19 @@
+use stopwatch::{Stopwatch};
+
 pub struct Player;
 
 use super::game_data_model::GameData;
 use super::game::Game;
 
 impl Player {
-    pub fn train(games_per_training: u32, threads: u32, minmax_depth_percentage: u8) {
+    pub fn train(games_per_training: u32, threads: u32, _minmax_depth_percentage: u8) {
 
 
+        let stopwatch = Stopwatch::start_new();
         let played_games: Vec<GameData> = Game::play_games(games_per_training, threads);
+        println!("Total games: {:?}\nTime: {:?}", played_games.len(), stopwatch.elapsed());    
+        println!("------------------------");
+        // println!("Game_data: {:?}", played_games);
     }
 
 }
