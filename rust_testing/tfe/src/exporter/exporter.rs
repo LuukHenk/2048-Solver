6bb143a0762d2247974ba6e28a51bc14c357b93f
luukhenk <luukperdaems@hotmail.com>
2023-01-25 18:31:20 +0100

    added board to json functions to exporter

--- rust_testing/tfe/src/exporter/exporter.rs
+++ rust_testing/tfe/src/exporter/exporter.rs
@@ -1,5 +1,7 @@
 use std::collections::VecDeque;
 
+use crate::game::direction::Direction;
+
 use super::pow_unsafe;
 use super::game::Game;
 use super::player::Player;
@@ -8,6 +10,9 @@ pub struct Exporter{
     player: Player
 }
 
+static SCORE_OBJECT_KEY: &'static str = "\"score\"";
+static PERFORMED_MOVE_OBJECT_KEY: &'static str = "\"performed move\"";
+static BOARD_OBJECT_KEY: &'static str = "\"board\"";
 static JSON_DATA_SEPERATOR: &'static str = ", ";
 static JSON_DICT_PAIR_SEPERATOR: &'static str = ": ";
 
@@ -41,33 +46,58 @@ impl Exporter{
 //         dict.push_str(&value);
 //         dict
 //     }
-    fn board_to_json(board: u64) -> String {
+    // fn __construct_board_object(board: Board) {
+
+    // }
+
+    fn __format_score_dict_object(score: u64) -> String {
+        Self::__construct_json_dict_pair(
+            SCORE_OBJECT_KEY.to_string(),
+            score.to_string()    
+        )
+    }
+
+    fn __format_performed_move_dict_object(performed_move: Direction) -> String {
+        Self::__construct_json_dict_pair(
+            PERFORMED_MOVE_OBJECT_KEY.to_string(),
+            performed_move.to_string()    
+        )
+    }
+
+    fn __format_board_dict_object(board: u64) -> String {
+        Self::__construct_json_dict_pair(
+            BOARD_OBJECT_KEY.to_string(),
+            Self::__board_to_json(board)
+        )
+    }
+
+    fn __board_to_json(board: u64) -> String {
         let mut rows: Vec<String> = Vec::with_capacity(4);
         for row_index in 0..4 {
             let row_value: u64 = board >> row_index * 16 & 0xFFFF;
-            rows.insert(0, Self::row_to_json(row_value));
+            rows.insert(0, Self::__row_to_json(row_value));
         }
         rows.join(JSON_DATA_SEPERATOR)
     }
 
-    fn row_to_json(row: u64) -> String {
+    fn __row_to_json(row: u64) -> String {
         let mut tiles: Vec<String> = Vec::with_capacity(4);
         for tile_index in 0..4 {
             let tile_value: u64 = row >> tile_index * 4 & 0xF;
             tiles.insert(0, pow_unsafe(tile_value).to_string());
         }
-        Self::construct_json_list(tiles)
+        Self::__construct_json_list(tiles)
     }
 
-    fn construct_json_list(list_items: Vec<String>) -> String {
-        vec![String::from("["), list_items.join(JSON_DATA_SEPERATOR), String::from("]")].join("")
+    fn __construct_json_list(list_items: Vec<String>) -> String {
+        vec!["[".to_string(), list_items.join(JSON_DATA_SEPERATOR), "]".to_string()].join("")
     }
 
-    fn construct_json_dict(dict_items: Vec<String>) -> String {
-        vec![String::from("{"), dict_items.join(JSON_DATA_SEPERATOR), String::from("}")].join("")
+    fn __construct_json_dict(dict_items: Vec<String>) -> String {
+        vec!["{".to_string(), dict_items.join(JSON_DATA_SEPERATOR), "}".to_string()].join("")
     }
 
-    fn construct_json_dict_pair(key: String, value: String) -> String {
+    fn __construct_json_dict_pair(key: String, value: String) -> String {
         vec![key, value].join(JSON_DICT_PAIR_SEPERATOR)
     }
 }
@@ -76,26 +106,53 @@ impl Exporter{
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_format_score_dict_object() {
+        let score: u64 = 10;
+        let expected_result: String = String::from(
+            "\"score\": 10"
+        );
+        assert_eq!(Exporter::__format_score_dict_object(score), expected_result);
+    }    
+
+    #[test]
+    fn test_format_performed_move_dict_object() {
+        let performed_move: Direction = Direction::Left;
+        let expected_result: String = String::from(
+            "\"performed move\": Left"
+        );
+        assert_eq!(Exporter::__format_performed_move_dict_object(performed_move), expected_result);
+    }    
+
+    #[test]
+    fn test_format_board_dict_object() {
+        let board: u64 = 0x1234_5678_4321_8765;
+        let expected_result: String = String::from(
+            "\"board\": [2, 4, 8, 16], [32, 64, 128, 256], [16, 8, 4, 2], [256, 128, 64, 32]"
+        );
+        assert_eq!(Exporter::__format_board_dict_object(board), expected_result);
+    }    
     #[test]
     fn test_board_to_json() {
         let board: u64 = 0x1234_5678_4321_8765;
         let expected_result: String = String::from(
             "[2, 4, 8, 16], [32, 64, 128, 256], [16, 8, 4, 2], [256, 128, 64, 32]"
         );
-        assert_eq!(Exporter::board_to_json(board), expected_result);
+        assert_eq!(Exporter::__board_to_json(board), expected_result);
     }
     #[test]
     fn test_row_to_json() {
         let board_row: u64 = 0x1234;
         let expected_result: String = String::from("[2, 4, 8, 16]");
-        assert_eq!(Exporter::row_to_json(board_row), expected_result);
+        assert_eq!(Exporter::__row_to_json(board_row), expected_result);
     }
 
     #[test]
     fn test_construct_json_dict_pair() {
         let key: String = String::from("hello");
         let value: String = String::from("goodbye");
-        assert_eq!(Exporter::construct_json_dict_pair(key, value), String::from("hello: goodbye"));
+        assert_eq!(Exporter::__construct_json_dict_pair(key, value), String::from("hello: goodbye"));
     }
 
     #[test]
@@ -103,7 +160,7 @@ mod tests {
         let data: &str = "hello";
         let object_data: Vec<String> = vec![String::from(data), String::from(data)];
         let expected_output: String = String::from("[hello, hello]");
-        assert_eq!(Exporter::construct_json_list(object_data), expected_output);
+        assert_eq!(Exporter::__construct_json_list(object_data), expected_output);
     }
 
     #[test]
@@ -111,7 +168,7 @@ mod tests {
         let data: &str = "hello: goodbye";
         let object_data: Vec<String> = vec![String::from(data), String::from(data)];
         let expected_output: String = String::from("{hello: goodbye, hello: goodbye}");
-        assert_eq!(Exporter::construct_json_dict(object_data), expected_output);
+        assert_eq!(Exporter::__construct_json_dict(object_data), expected_output);
     }
 
 }
