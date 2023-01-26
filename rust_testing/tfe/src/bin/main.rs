d627943b5f113f1fc9ca18a51a16bdabbd9474d0
luukhenk <luukperdaems@hotmail.com>
2023-01-26 21:20:19 +0100

    reworked trainer and main

--- rust_testing/tfe/src/bin/main.rs
+++ rust_testing/tfe/src/bin/main.rs
@@ -1,10 +1,13 @@
-use tfe::{Player, Trainer, export_games_to_json_file};
+use tfe::{Trainer, export_games_to_json_file};
 
 fn main() {
-    let saving_path: String = "../../data/results.json".to_string();
-    let player: Player = Player::new();
-    let mut trainer: Trainer = Trainer::new(player, 1);
-    trainer.train();
-    export_games_to_json_file(trainer.copy_top_games(), saving_path);
+    let saving_path: &str = "../../data/results.json";
+    let games_per_trainings_round: usize = 100;
+    let total_trainings_rounds: usize = 5;
+    let top_games: usize = 1;
+    let mut trainer: Trainer = Trainer::new(top_games);
 
+    trainer.train(games_per_trainings_round, total_trainings_rounds);
+    
+    export_games_to_json_file(trainer.copy_top_games(), saving_path.to_string());
 }
--- rust_testing/tfe/src/trainer/trainer.rs
+++ rust_testing/tfe/src/trainer/trainer.rs
@@ -9,21 +9,20 @@ pub struct Trainer{
 }
 
 impl Trainer{
-    pub fn new(player: Player, top_games: usize) -> Trainer {
+    pub fn new(top_games: usize) -> Trainer {
+        let player: Player = Player::new();
         Trainer {player, top_games}
     }
 
 
-    pub fn train(&mut self) {
-        let initial_games: usize = 100;
-        let total_trainings_rounds: usize = 5;
+    pub fn train(&mut self, games_per_trainings_round: usize, total_trainings_rounds: usize) {
 
         println!("Playing initial games");
-        self.__play_initial_games(initial_games);
+        self.__play_initial_games(games_per_trainings_round);
 
         for trainings_round_index in 0.. total_trainings_rounds {
             println!("Trainings round {}", trainings_round_index+1);
-            self.__trainings_round(initial_games)
+            self.__trainings_round(games_per_trainings_round)
         }
     }
     
@@ -37,8 +36,8 @@ impl Trainer{
         self.__cleanup();
     }
 
-    fn __trainings_round(&mut self, initial_games: usize) {
-        let retries: usize = initial_games/self.top_games;
+    fn __trainings_round(&mut self, games_per_trainings_round: usize) {
+        let retries: usize = games_per_trainings_round/self.top_games;
         for game_index in 0 .. self.top_games {
             self.player.retry_game(game_index, retries);
         }
