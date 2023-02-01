3e5e3bfb4f4240e1235753fb4c9d0b860bd791d2
luukhenk <luukperdaems@hotmail.com>
2023-02-01 15:47:43 +0100

    added previous and next button

--- 2048/pyqt_game_displayer/build/lib/game_displayer/game/game_controls_widget.py
+++ 2048/pyqt_game_displayer/build/lib/game_displayer/game/game_controls_widget.py
@@ -10,22 +10,55 @@ from PySide6.QtCore import Qt, Signal, Slot
 
 class GameControlsWidget(QWidget):
     
-    startClicked = Signal()
+    startButtonClicked = Signal()
+    previousMoveButtonClicked = Signal()
+    nextMoveButtonClicked = Signal()
     
     START_TEXT: Final[str] = "Start"
-    PAUSE_TEXT: Final[str] = "Pause"
+    PREVIOUS_MOVE_TEXT: Final[str] = "<"
+    NEXT_MOVE_TEXT: Final[str] = ">"
     
     def __init__(self) -> None:
         super().__init__()
+        self.__previous_move_button = self.__create_previous_move_button()
+        self.__next_move_button = self.__create_next_move_button()
         self.__start_button = self.__create_start_button()
+        
         self.__create_game_controls()
+    
+    def enable_all_controls(self) -> None:
+        self.__previous_move_button.setEnabled(True)
+        self.__next_move_button.setEnabled(True)
+        self.__start_button.setEnabled(True)
+        
+    def on_game_start(self) -> None:
+        self.__previous_move_button.setDisabled(True)
+        self.__next_move_button.setEnabled(True)
+        self.__start_button.setEnabled(True)
+    
+    def on_game_end(self) -> None:
+        self.__previous_move_button.setEnabled(True)
+        self.__next_move_button.setDisabled(True)
+        self.__start_button.setDisabled(True)
         
     def __create_game_controls(self) -> None:
         layout = QHBoxLayout(self)
+        layout.addWidget(self.__previous_move_button)
         layout.addWidget(self.__start_button)
+        layout.addWidget(self.__next_move_button)
         layout.setAlignment(Qt.AlignCenter)
         
     def __create_start_button(self) -> QPushButton:
-        button = QPushButton(self.START_TEXT)
-        button.clicked.connect(self.startClicked)
-        return button
+        start_button = QPushButton(self.START_TEXT)
+        start_button.clicked.connect(self.startButtonClicked)
+        return start_button
+    
+    def __create_previous_move_button(self) -> QPushButton:
+        previous_move_button = QPushButton(self.PREVIOUS_MOVE_TEXT)
+        previous_move_button.clicked.connect(self.previousMoveButtonClicked)
+        return previous_move_button
+    
+    def __create_next_move_button(self) -> QPushButton:
+        next_move_button = QPushButton(self.NEXT_MOVE_TEXT)
+        next_move_button.clicked.connect(self.nextMoveButtonClicked)
+        return next_move_button
--- 2048/pyqt_game_displayer/build/lib/game_displayer/game/game_widget.py
+++ 2048/pyqt_game_displayer/build/lib/game_displayer/game/game_widget.py
@@ -19,6 +19,7 @@ class GameWidget(QWidget):
     def __init__(self, boards: BoardType) -> None:
         super().__init__()
         self.__boards = boards
+        self.__current_board_index = 0
         self.__game_controls = self.__setup_game_controls()
         self.__board_widget = BoardWidget(self.BOARD_SIZE)
         self.__setup_game_layout()
@@ -26,26 +27,59 @@ class GameWidget(QWidget):
         self.__thread_pool = QThreadPool()
         self.__thread_pool.setMaxThreadCount(1)
 
-    def __display_game(self) -> None:
+    def __display_full_game(self) -> None:
         """NOTE that this should be done on a different thread"""
-        for board in self.__boards:
-            self.__board_widget.boardUpdated.emit(board)
+        for board_index in range(self.__current_board_index, len(self.__boards)):
+            self.__current_board_index = board_index
+            self.__update_displayed_board()
             sleep(0.2)
-        self.__game_controls.setEnabled(True)
+        self.__game_controls.setEnabled(True) 
+        self.__game_controls.on_game_end()
             
     def __setup_game_layout(self):
         layout = QVBoxLayout(self)
         layout.addWidget(self.__board_widget)
         layout.addWidget(self.__game_controls)
         layout.setAlignment(Qt.AlignCenter)
+        self.__update_displayed_board()
     
     def __setup_game_controls(self) -> GameControlsWidget:
         game_controls = GameControlsWidget()
-        game_controls.startClicked.connect(self.__on_start_clicked)
+        game_controls.startButtonClicked.connect(self.__on_start_clicked)
+        game_controls.previousMoveButtonClicked.connect(self.__on_previous_button_clicked)
+        game_controls.nextMoveButtonClicked.connect(self.__on_next_button_clicked)
+        game_controls.on_game_start()
         return game_controls
+    
+    def __update_displayed_board(self) -> None:
+        current_board = self.__boards[self.__current_board_index] 
+        self.__board_widget.boardUpdated.emit(current_board)
         
     @Slot()
     def __on_start_clicked(self) -> None:
-        self.__game_controls.setDisabled(True)
-        self.__thread_pool.start(self.__display_game)
+        self.__game_controls.setDisabled(True) # Disabling controls is needed for threading!
+        self.__thread_pool.start(self.__display_full_game)
     
+    @Slot()
+    def __on_previous_button_clicked(self) -> None:
+        self.__current_board_index -= 1
+        self.__update_displayed_board()
+        
+        if self.__current_board_index == 0:
+            self.__game_controls.on_game_start()
+            return
+        self.__game_controls.enable_all_controls()
+            
+
+    @Slot()
+    def __on_next_button_clicked(self) -> None:
+        self.__current_board_index += 1
+        self.__update_displayed_board()
+        
+        if self.__current_board_index == len(self.__boards) - 1:
+            self.__game_controls.on_game_end()
+            return
+        self.__game_controls.enable_all_controls()
+        
+
+        
