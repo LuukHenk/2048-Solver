507b8814a693d0b4fe4196d92dabf268f8b1822f
luukhenk <luukperdaems@hotmail.com>
2023-02-01 19:02:44 +0100

    bugfix in display

--- 2048/pyqt_game_displayer/src/game_displayer/game/game_widget.py
+++ 2048/pyqt_game_displayer/src/game_displayer/game/game_widget.py
@@ -1,5 +1,5 @@
 
-from time import sleep  
+from time import sleep
 from typing import Final
 from PySide6.QtWidgets import (
     QVBoxLayout,
@@ -12,10 +12,10 @@ from game_displayer.game.game_controls_widget import GameControlsWidget
 from game_displayer.game.game_info_bar_widget import GameInfoBarWidget
 
 class GameWidget(QWidget):
-    
+
     BOARD_SIZE: Final[int] = 4
-    
-    
+
+
     def __init__(self, game: dict) -> None:
         super().__init__()
         self.__game = game
@@ -24,7 +24,7 @@ class GameWidget(QWidget):
         self.__game_info_bar = GameInfoBarWidget()
         self.__board_widget = BoardWidget(self.BOARD_SIZE)
         self.__setup_game_layout()
-        
+
         self.__thread_pool = QThreadPool()
         self.__thread_pool.setMaxThreadCount(1)
 
@@ -34,9 +34,9 @@ class GameWidget(QWidget):
             self.__current_board_index = board_index
             self.__update_displayed_board()
             sleep(0.2)
-        self.__game_controls.setEnabled(True) 
+        self.__game_controls.setEnabled(True)
         self.__game_controls.set_controls_for_game_end()
-            
+
     def __setup_game_layout(self):
         layout = QVBoxLayout(self)
         layout.addWidget(self.__game_info_bar)
@@ -44,7 +44,7 @@ class GameWidget(QWidget):
         layout.addWidget(self.__game_controls)
         layout.setAlignment(Qt.AlignCenter)
         self.__update_displayed_board()
-    
+
     def __setup_game_controls(self) -> GameControlsWidget:
         game_controls = GameControlsWidget()
         game_controls.startButtonClicked.connect(self.__on_start_clicked)
@@ -54,39 +54,39 @@ class GameWidget(QWidget):
         game_controls.goToEndButtonClicked.connect(self.__on_go_to_end_button_clicked)
         game_controls.set_controls_for_new_game()
         return game_controls
-    
+
     def __update_displayed_board(self) -> None:
-        current_move = self.__game[self.__current_board_index] 
+        current_move = self.__game[self.__current_board_index]
         self.__board_widget.boardUpdated.emit(current_move["board"])
         self.__game_info_bar.scoreUpdate.emit(current_move["score"])
-        self.__game_info_bar.latestMovementUpdate.emit(current_move["score"])
-        
+        self.__game_info_bar.latestMovementUpdate.emit(current_move["performed move"])
+
     @Slot()
     def __on_start_clicked(self) -> None:
         self.__game_controls.setDisabled(True) # Disabling controls is needed for threading!
         self.__thread_pool.start(self.__display_full_game)
-    
+
     @Slot()
     def __on_previous_button_clicked(self) -> None:
         self.__current_board_index -= 1
         self.__update_displayed_board()
-        
+
         if self.__current_board_index == 0:
             self.__game_controls.set_controls_for_new_game()
             return
         self.__game_controls.enable_all_controls()
-            
+
 
     @Slot()
     def __on_next_button_clicked(self) -> None:
         self.__current_board_index += 1
         self.__update_displayed_board()
-        
+
         if self.__current_board_index == len(self.__game) - 1:
             self.__game_controls.set_controls_for_game_end()
             return
         self.__game_controls.enable_all_controls()
-    
+
     @Slot()
     def __on_go_to_start_button_clicked(self) -> None:
         self.__current_board_index = 0
